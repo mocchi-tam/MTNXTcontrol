0fab04cd254ead09fa2006490b09a0615b98b0d0
Mocchi tam <onlyephem@gmail.com>
2017-11-01 14:43:46 +0900

    Update run.py

--- run.py
+++ run.py
@@ -12,7 +12,9 @@ class NXTControl():
     def spin_around(self, b, flag):
         m_right = nxtm.Motor(b, nxtm.PORT_C)
         if flag:
-            m_right.turn(-10, 720)
+            #m_right.turn(10, 360)
+            milsec = random.uniform(3.0,5.0)
+            time.sleep(milsec)
         else:
             m_right.turn(60, 720)
     
@@ -27,9 +29,9 @@ class NXTControl():
             while(True):
                 elapsed_time = time.time() - start
                 if elapsed_time < time_bomb:
-                    flag = False
-                else:
                     flag = True
+                else:
+                    flag = False
                     start = time.time()
                     time_bomb = random.randint(25,35)
                     
